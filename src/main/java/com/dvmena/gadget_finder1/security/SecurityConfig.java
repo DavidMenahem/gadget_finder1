9554732d0aafb361c1bef1b79114b071e375b3b4
dvmena36 <dvmena39@gmail.com>
2023-08-28 10:06:57 +0300

    added root path to secrity filter chain

--- src/main/java/com/dvmena/gadget_finder1/security/SecurityConfig.java
+++ src/main/java/com/dvmena/gadget_finder1/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig{
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
         return  http.csrf().disable()
                 .authorizeHttpRequests(
-                        (auth)->auth.requestMatchers("/login/**")
+                        (auth)->auth.requestMatchers("/login/**","")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated())
