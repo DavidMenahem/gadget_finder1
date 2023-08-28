abfeccd71eda786b1041c8a54801c92c3a6f6f66
dvmena36 <dvmena39@gmail.com>
2023-08-28 09:44:46 +0300

    changed login url to blank

--- src/main/java/com/dvmena/gadget_finder1/controller/AuthController.java
+++ src/main/java/com/dvmena/gadget_finder1/controller/AuthController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/login")
+@RequestMapping
 public class AuthController {
 
     private final LoginService loginService;
