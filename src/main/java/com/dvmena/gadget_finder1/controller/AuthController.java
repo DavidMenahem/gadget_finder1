c367d38dfc31e5500536504ab561f8d1df8cf3a8
dvmena36 <dvmena39@gmail.com>
2023-08-29 10:40:56 +0300

    urls

--- src/main/java/com/dvmena/gadget_finder1/controller/AuthController.java
+++ src/main/java/com/dvmena/gadget_finder1/controller/AuthController.java
@@ -5,6 +5,10 @@ import com.dvmena.gadget_finder1.model.Register;
 import com.dvmena.gadget_finder1.service.LoginService;
 import com.dvmena.gadget_finder1.service.RegisterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
@@ -18,15 +22,22 @@ public class AuthController {
     private final RegisterService registerService;
 
     @PostMapping("/login")
-    public String login(@RequestBody Login login){
+    public ResponseEntity<String> login(@RequestBody Login login){
+        // Create a HttpHeaders object.
+        HttpHeaders headers = new HttpHeaders();
+
+        // Set the content type header to application/json.
+        headers.setContentType(MediaType.APPLICATION_JSON);
         Optional<Register> register = registerService.get(login.getEmail());
         if(register.isPresent()){
             System.out.println(login.getPassword());
             if(register.get().getPassword().equals(login.getPassword())) {
-                return loginService.login(login.getEmail(), login.getPassword());
+
+                String token =  loginService.login(login.getEmail(), login.getPassword());
+                ResponseEntity<String> response = new ResponseEntity<>(token,headers, HttpStatus.OK);
             }
         }
-        return "user not found";
+        return new ResponseEntity<String>("user not found",headers,HttpStatus.OK);
     }
     @PostMapping("/register")
     public Register register(@RequestBody Register register){
