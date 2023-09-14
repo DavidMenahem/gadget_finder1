de31e84758cf185ba6a48ab22048b05ae4da463c
dvmena36 <dvmena39@gmail.com>
2023-09-14 10:15:23 +0300

    changed login api

--- src/main/java/com/dvmena/gadget_finder1/controller/RegisterController.java
+++ src/main/java/com/dvmena/gadget_finder1/controller/RegisterController.java
@@ -1,8 +1,10 @@
 package com.dvmena.gadget_finder1.controller;
 
 import com.dvmena.gadget_finder1.exceptions.ApplicationException;
+import com.dvmena.gadget_finder1.model.Login;
 import com.dvmena.gadget_finder1.model.Response;
 import com.dvmena.gadget_finder1.model.Register;
+import com.dvmena.gadget_finder1.service.LoginService;
 import com.dvmena.gadget_finder1.service.RegisterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.aot.generate.AccessControl;
@@ -15,7 +17,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RegisterController {
     private final RegisterService registerService;
+    private final LoginService loginService;
 
+    @PostMapping("/login")
+    public Response login(@RequestBody Login login){
+        return loginService.login(login);
+    }
     @PostMapping("/register")
     public Response register(@RequestBody Register register){
 
